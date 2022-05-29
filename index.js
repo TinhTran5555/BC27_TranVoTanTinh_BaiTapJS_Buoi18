631b0eae91d36d87e7352e51764ddbb69d826c44
TinhTran5555 <tranvotantinh@gmail.com>
2022-05-29 18:13:17 +0700

    Fix 2022/05/09

--- index.js
+++ index.js
@@ -70,27 +70,39 @@ function timSoDuongNhoNhat() {
       soDuong.push(nums[i]);
     }
   }
+  // Tạo biến soNhoNhat để xét cho trường hợp không có số dương nào trong mảng
+  var soNhoNhat = -1
   // B1 Tạo biến minSoDuong và gán cho giá trị đầu tiền của mảng soDuong
   var minSoDuong = soDuong[0];
   // b2 Duyệt và kiểm tra mảng từ vị trí thứ 2, nếu số đó nhỏ hơn minSoDuong thì ta sẽ gán lại cho minSoDuong
+  // var 
   for (var index = 1; index < soDuong.length; index++) {
-    if (soDuong[0] < minSoDuong) {
+    if (soDuong[index] < minSoDuong) {
       minSoDuong = soDuong[index];
+      soNhoNhat = minSoDuong
     }
   }
+  console.log(soNhoNhat);
+  if (soNhoNhat === -1 ) {
+    var divHienSoDuongNhoNhat = document.getElementById("divHienSoDuongNhoNhat");
+  divHienSoDuongNhoNhat.style.display = "block";
+  divHienSoDuongNhoNhat.innerHTML = `
+      <p>Không có số dương nào trong mảng</p>
+    `;
+  } else {
   var divHienSoDuongNhoNhat = document.getElementById("divHienSoDuongNhoNhat");
   divHienSoDuongNhoNhat.style.display = "block";
   divHienSoDuongNhoNhat.innerHTML = `
       <p>Số nhỏ nhất: ${minSoDuong}</p>
     `;
-}
+}}
 // Bài 5
 // B1 Viết hàm kiểm tra số chẵn để trả về hai kết quả khác nhau
 function kiemTraCoSoChan() {
   // B2 Viết hàm tìm số chẵn cuối cùng bến dưới rồi gán vào biến số chẵn
   var soChan = timSoChan();
-  console.log(soChan);
-  if (soChan === -1) {
+  Xét trường hợp khi không có bất kì số chẵn nào trong mảng
+  if (soChan === -1 || soChan===0) {
     // B3 Hiện thị Kết quả
     var divHienSoChanCuoiCung = document.getElementById(
       "divHienSoChanCuoiCung"
@@ -111,7 +123,9 @@ function kiemTraCoSoChan() {
 }
 // Viết hàm tìm số chẵn
 function timSoChan() {
+  // B1 Tạo biến soChan
   var soChan = -1;
+  // Tạo vòng lặp
   for (var index = 0; index < nums.length; index++) {
     if (nums[index] % 2 == 0) {
       soChan = nums[index];
@@ -120,7 +134,6 @@ function timSoChan() {
 
   return soChan;
 }
-
 // Bài 6
 function doiViTri() {
   //   B1 Dom tới các input để lấy giá trị, đồng thời tạo một biến tạm thời.
@@ -216,7 +229,7 @@ function soSanhAmDuong() {
   for (var i = 0; i < nums.length; i++) {
     if (nums[i] > 0) {
       soLuongSoDuong++;
-    } else {
+    } else if (nums[i] < 0){
       soLuongSoAm++;
     }
   }
